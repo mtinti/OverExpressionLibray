ab33c399c15e2f842b9d12e4f7fcf63e2ea61d9d
mtinti <michele.tinti@gmail.com>
2020-02-18 12:25:33 +0000

    allow myqstat.sh for username variable

--- myqstat.sh
+++ myqstat.sh
@@ -8,5 +8,5 @@ qstat -u mtinti -xml | tr '\n' ' ' | sed 's#<job_list[^>]*>#\n#g' | sed 's#<[^>]
 
 echo '----'
 echo 'tot:' 
-qstat -u mtinti -xml | grep JB_name  | wc -l
+qstat -u $1 -xml | grep JB_name  | wc -l
 
